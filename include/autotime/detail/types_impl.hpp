b62d599a42ae48eb199acc72749f31b94b7f0cb1
Matt <mattgruenke@users.noreply.github.com>
2022-08-13 15:19:43 -0400

    Added detail/types_impl.hpp - Low-level implementation types.

---
+++ include/autotime/detail/types_impl.hpp
@@ -0,0 +1,38 @@
+////////////////////////////////////////////////////////////////////////////////
+//  Copyright Matthew A. Gruenke 2022.
+//
+//  Distributed under the Boost Software License, Version 1.0.
+//  (See accompanying file LICENSE or copy at
+//   http://www.boost.org/LICENSE_1_0.txt)
+//
+//! Low-level implementation types.
+/*! @file
+
+    This file is not meant to be included, directly.
+*/
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef AUTOTIME_DETAIL_TYPES_IMPL_HPP
+#define AUTOTIME_DETAIL_TYPES_IMPL_HPP
+
+
+namespace autotime
+{
+
+
+namespace detail
+{
+
+
+    // Used by non-inline functions which accept pointer-to-member parameters.
+class ProxyType {};
+
+
+} // namespace detail
+
+
+} // namespace autotime
+
+
+#endif // ndef AUTOTIME_DETAIL_TYPES_IMPL_HPP
+
