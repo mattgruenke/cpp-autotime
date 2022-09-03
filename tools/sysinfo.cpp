dba788993068292b37b9fe06c5d152dd4f08facc
Matt <mattgruenke@users.noreply.github.com>
2022-09-03 06:43:12 -0400

    Minor file header formatting fix.

--- tools/sysinfo.cpp
+++ tools/sysinfo.cpp
@@ -2,7 +2,7 @@
 //  Copyright Matthew A. Gruenke 2022.
 //
 //  Distributed under the Boost Software License, Version 1.0.
-//  (See accompanying file LICENSE or copy at //   http://www.boost.org/LICENSE_1_0.txt)
+//  (See accompanying file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 //! Info & diagnostic tool for determining system-dependent parameters.
 /*! @file
