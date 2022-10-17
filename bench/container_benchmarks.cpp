7838c7c4ab5c944edf4567d873810329c1e9eb5c
Matt <mattgruenke@users.noreply.github.com>
2022-10-17 01:28:42 -0400

    Reorganized container_benchmarks.cpp, placing generics at the top of the file.

--- bench/container_benchmarks.cpp
+++ bench/container_benchmarks.cpp
@@ -37,6 +37,10 @@ namespace bench
 {
 
 
+////////////////////////////////
+// Test data generation:
+////////////////////////////////
+
 template< typename elem_t > static elem_t MakeElement( size_t i )
 {
     return static_cast< elem_t >( i );
@@ -99,6 +103,17 @@ template< typename elem_t > std::shared_ptr< elem_t[] > MakeData( size_t n )
 }
 
 
+////////////////////////////////
+// Dummy containers:
+////////////////////////////////
+
+template< typename container_t > container_t &Writable();
+
+
+////////////////////////////////
+// Insert generics:
+////////////////////////////////
+
 template< typename element_t >
     inline void InsertElement( std::set< element_t > &set, element_t value )
 {
@@ -130,15 +145,6 @@ template< typename container_t >
 
 
 template< typename container_t >
-    container_t PushBack( const typename container_t::value_type *data, size_t n )
-{
-    container_t result;
-    for (size_t i = 0; i < n; ++i) result.push_back( data[i] );
-    return result;
-}
-
-
-template< typename container_t >
     static Durations InsertTimer(
         std::shared_ptr< typename container_t::value_type[] > data, size_t n, int num_iters )
 {
@@ -164,63 +170,9 @@ template< typename container_t >
 }
 
 
-////////////////////////////////////////////////////////////
-// Category::std_set:
-////////////////////////////////////////////////////////////
-
-template<> autotime::BenchTimers MakeTimers< Benchmark::set_int32_insert16 >()
-{
-    return MakeInsertTimers< std::set< int32_t > >( 1 << 4 );
-}
-
-
-template<> autotime::BenchTimers MakeTimers< Benchmark::set_int32_insert256 >()
-{
-    return MakeInsertTimers< std::set< int32_t > >( 1 << 8 );
-}
-
-
-template<> autotime::BenchTimers MakeTimers< Benchmark::set_int32_insert4k >()
-{
-    return MakeInsertTimers< std::set< int32_t > >( 1 << 12 );
-}
-
-
-template<> autotime::BenchTimers MakeTimers< Benchmark::set_int32_insert64k >()
-{
-    return MakeInsertTimers< std::set< int32_t > >( 1 << 16 );
-}
-
-
-template<> autotime::BenchTimers MakeTimers< Benchmark::set_int32_insert1M >()
-{
-    return MakeInsertTimers< std::set< int32_t > >( 1 << 20 );
-}
-
-
-template<> autotime::BenchTimers MakeTimers< Benchmark::set_int64_insert4k >()
-{
-    return MakeInsertTimers< std::set< int64_t > >( 1 << 12 );
-}
-
-
-template<> autotime::BenchTimers MakeTimers< Benchmark::set_float_insert4k >()
-{
-    return MakeInsertTimers< std::set< float > >( 1 << 12 );
-}
-
-
-template<> autotime::BenchTimers MakeTimers< Benchmark::set_double_insert4k >()
-{
-    return MakeInsertTimers< std::set< double > >( 1 << 12 );
-}
-
-
-template<> autotime::BenchTimers MakeTimers< Benchmark::set_string_insert4k >()
-{
-    return MakeInsertTimers< std::set< std::string > >( 1 << 12 );
-}
-
+////////////////////////////////
+// Counting generics:
+////////////////////////////////
 
 template< typename container_t >
     static size_t Count( const container_t &c )
@@ -279,6 +231,264 @@ template< typename container_t >
 }
 
 
+////////////////////////////////
+// Find generics:
+////////////////////////////////
+
+template< typename element_t >
+    inline bool HasElement( const std::set< element_t > &set, element_t value )
+{
+    return set.find( value ) != set.end();
+}
+
+
+template< typename element_t >
+    inline bool HasElement( const std::vector< element_t > &v, element_t value )
+{
+    return std::find( v.begin(), v.end(), value ) != v.end();
+}
+
+
+template< typename container_t >
+    bool Find( const container_t &c, typename container_t::value_type value )
+{
+    return c.find( value ) != c.end();
+}
+
+
+template< typename container_t >
+    static Durations FindTimer(
+        std::shared_ptr< container_t > container,
+        std::shared_ptr< typename container_t::value_type[] > data,
+        int data_size,
+        int num_iters )
+{
+    const container_t &c = *container;
+
+    unsigned int count = 0;
+    TimePoints start_times = Start();
+    for (int i = 0; i < num_iters; ++i)
+    {
+#if 1
+        using element_t = typename container_t::value_type;
+        if (HasElement< element_t >( c, data[i % data_size] )) ++count;
+#else
+        if (Find( c, data[i % data_size] )) ++count;
+#endif
+    }
+    CountResult = count;
+
+    return End( start_times );
+}
+
+
+template< typename container_t >
+    static autotime::BenchTimers MakeFindTimers( bool sort, size_t n )
+{
+    using namespace std::placeholders;
+    using value_t = typename container_t::value_type;
+    std::shared_ptr< value_t[] > data = MakeData< value_t >( n );
+    std::shared_ptr< container_t > container{
+        new container_t{ Insert< container_t >( data.get(), n ) } };
+
+    if (sort) std::sort( data.get(), data.get() + n );
+
+    return { std::bind( &FindTimer< container_t >, container, data, n, _1 ), nullptr };
+}
+
+
+////////////////////////////////
+// Copy generics:
+////////////////////////////////
+
+template< typename container_t >
+    static Durations CopyTimer( std::shared_ptr< container_t > container, int num_iters )
+{
+    const container_t &src = *container;
+    container_t &dst = Writable< container_t >();
+
+    std::function< Durations() > f = [src, &dst]()
+        {
+            container_t tmp;
+            tmp.swap( dst );
+
+            TimePoints start_times = Start();
+            dst = src;
+            return End( start_times );
+        };
+
+    return Time( f, num_iters );
+}
+
+
+template< typename container_t >
+    static Durations CopyOverhead( std::shared_ptr< container_t > container, int num_iters )
+{
+    const container_t &src = *container;
+    container_t &dst = Writable< container_t >();
+    dst.clear();
+
+    std::function< void() > f = [src, dst]()
+        {
+            if (src.empty() || !dst.empty()) std::terminate();
+        };
+
+    return Time( f, num_iters );
+}
+
+
+template< typename container_t >
+    static autotime::BenchTimers MakeCopyTimers( size_t n )
+{
+    using namespace std::placeholders;
+    using value_t = typename container_t::value_type;
+    std::shared_ptr< value_t[] > data = MakeData< value_t >( n );
+    std::shared_ptr< container_t > container{
+        new container_t{ Insert< container_t >( data.get(), n ) } };
+
+    return {
+            std::bind( &CopyTimer< container_t >, container, _1 ),
+            std::bind( &CopyOverhead< container_t >, container, _1 )
+        };
+}
+
+
+////////////////////////////////
+// Destroy generics:
+////////////////////////////////
+
+template< typename container_t >
+    static Durations DestroyTimer( std::shared_ptr< container_t > container, int num_iters )
+{
+    const container_t &src = *container;
+    container_t &dst = Writable< container_t >();
+
+    std::function< Durations() > f = [src, &dst]()
+        {
+            dst = src;
+
+            TimePoints start_times = Start();
+            {
+                container_t tmp;
+                tmp.swap( dst );
+            }
+            return End( start_times );
+        };
+
+    return Time( f, num_iters );
+}
+
+
+template< typename container_t >
+    static autotime::BenchTimers MakeDestroyTimers( size_t n )
+{
+    using namespace std::placeholders;
+    using value_t = typename container_t::value_type;
+    std::shared_ptr< value_t[] > data = MakeData< value_t >( n );
+    std::shared_ptr< container_t > container{
+        new container_t{ Insert< container_t >( data.get(), n ) } };
+
+    return {
+            std::bind( &DestroyTimer< container_t >, container, _1 ),
+            std::bind( &CopyOverhead< container_t >, container, _1 )
+        };
+}
+
+
+////////////////////////////////////////////////////////////
+// Category::std_set:
+////////////////////////////////////////////////////////////
+
+std::set< int32_t > Set_int32;
+std::set< int64_t > Set_int64;
+std::set< float > Set_float;
+std::set< double > Set_double;
+std::set< std::string > Set_string;
+
+
+template<> std::set< int32_t > &Writable< std::set< int32_t > >()
+{
+    return Set_int32;
+}
+
+
+template<> std::set< int64_t > &Writable< std::set< int64_t > >()
+{
+    return Set_int64;
+}
+
+
+template<> std::set< float > &Writable< std::set< float > >()
+{
+    return Set_float;
+}
+
+
+template<> std::set< double > &Writable< std::set< double > >()
+{
+    return Set_double;
+}
+
+
+template<> std::set< std::string > &Writable< std::set< std::string > >()
+{
+    return Set_string;
+}
+
+template<> autotime::BenchTimers MakeTimers< Benchmark::set_int32_insert16 >()
+{
+    return MakeInsertTimers< std::set< int32_t > >( 1 << 4 );
+}
+
+
+template<> autotime::BenchTimers MakeTimers< Benchmark::set_int32_insert256 >()
+{
+    return MakeInsertTimers< std::set< int32_t > >( 1 << 8 );
+}
+
+
+template<> autotime::BenchTimers MakeTimers< Benchmark::set_int32_insert4k >()
+{
+    return MakeInsertTimers< std::set< int32_t > >( 1 << 12 );
+}
+
+
+template<> autotime::BenchTimers MakeTimers< Benchmark::set_int32_insert64k >()
+{
+    return MakeInsertTimers< std::set< int32_t > >( 1 << 16 );
+}
+
+
+template<> autotime::BenchTimers MakeTimers< Benchmark::set_int32_insert1M >()
+{
+    return MakeInsertTimers< std::set< int32_t > >( 1 << 20 );
+}
+
+
+template<> autotime::BenchTimers MakeTimers< Benchmark::set_int64_insert4k >()
+{
+    return MakeInsertTimers< std::set< int64_t > >( 1 << 12 );
+}
+
+
+template<> autotime::BenchTimers MakeTimers< Benchmark::set_float_insert4k >()
+{
+    return MakeInsertTimers< std::set< float > >( 1 << 12 );
+}
+
+
+template<> autotime::BenchTimers MakeTimers< Benchmark::set_double_insert4k >()
+{
+    return MakeInsertTimers< std::set< double > >( 1 << 12 );
+}
+
+
+template<> autotime::BenchTimers MakeTimers< Benchmark::set_string_insert4k >()
+{
+    return MakeInsertTimers< std::set< std::string > >( 1 << 12 );
+}
+
+
 template<> autotime::BenchTimers MakeTimers< Benchmark::set_int32_iterate16 >()
 {
     return MakeCountTimers< std::set< int32_t > >( 1 << 4 );
@@ -330,69 +540,6 @@ template<> autotime::BenchTimers MakeTimers< Benchmark::set_double_iterate64k >(
 template<> autotime::BenchTimers MakeTimers< Benchmark::set_string_iterate64k >()
 {
     return MakeCountTimers< std::set< std::string > >( 1 << 16 );
-}
-
-
-template< typename element_t >
-    inline bool HasElement( const std::set< element_t > &set, element_t value )
-{
-    return set.find( value ) != set.end();
-}
-
-
-template< typename element_t >
-    inline bool HasElement( const std::vector< element_t > &v, element_t value )
-{
-    return std::find( v.begin(), v.end(), value ) != v.end();
-}
-
-
-
-template< typename container_t >
-    bool Find( const container_t &c, typename container_t::value_type value )
-{
-    return c.find( value ) != c.end();
-}
-
-
-template< typename container_t >
-    static Durations FindTimer(
-        std::shared_ptr< container_t > container,
-        std::shared_ptr< typename container_t::value_type[] > data,
-        int data_size,
-        int num_iters )
-{
-    const container_t &c = *container;
-
-    unsigned int count = 0;
-    TimePoints start_times = Start();
-    for (int i = 0; i < num_iters; ++i)
-    {
-#if 1
-        using element_t = typename container_t::value_type;
-        if (HasElement< element_t >( c, data[i % data_size] )) ++count;
-#else
-        if (Find( c, data[i % data_size] )) ++count;
-#endif
-    }
-    CountResult = count;
-
-    return End( start_times );
-}
-
-
-template< typename container_t >
-    static autotime::BenchTimers MakeFindTimers( bool sort, size_t n )
-{
-    using namespace std::placeholders;
-    using value_t = typename container_t::value_type;
-    std::shared_ptr< value_t[] > data = MakeData< value_t >( n );
-    std::shared_ptr< container_t > container{
-        new container_t{ Insert< container_t >( data.get(), n ) } };
-
-    if (sort) std::sort( data.get(), data.get() + n );
-
-    return { std::bind( &FindTimer< container_t >, container, data, n, _1 ), nullptr };
 }
 
 
@@ -456,151 +603,6 @@ template<> autotime::BenchTimers MakeTimers< Benchmark::set_string_find64k >()
 }
 
 
-template< typename container_t > container_t &Writable();
-
-
-std::set< int32_t > Set_int32;
-
-
-template<> std::set< int32_t > &Writable< std::set< int32_t > >()
-{
-    return Set_int32;
-}
-
-
-std::set< int64_t > Set_int64;
-
-
-template<> std::set< int64_t > &Writable< std::set< int64_t > >()
-{
-    return Set_int64;
-}
-
-
-std::set< float > Set_float;
-
-
-template<> std::set< float > &Writable< std::set< float > >()
-{
-    return Set_float;
-}
-
-
-std::set< double > Set_double;
-
-
-template<> std::set< double > &Writable< std::set< double > >()
-{
-    return Set_double;
-}
-
-
-std::set< std::string > Set_string;
-
-
-template<> std::set< std::string > &Writable< std::set< std::string > >()
-{
-    return Set_string;
-}
-
-
-std::vector< int32_t > Vector_int32;
-
-
-template<> std::vector< int32_t > &Writable< std::vector< int32_t > >()
-{
-    return Vector_int32;
-}
-
-
-std::vector< int64_t > Vector_int64;
-
-
-template<> std::vector< int64_t > &Writable< std::vector< int64_t > >()
-{
-    return Vector_int64;
-}
-
-
-std::vector< float > Vector_float;
-
-
-template<> std::vector< float > &Writable< std::vector< float > >()
-{
-    return Vector_float;
-}
-
-
-std::vector< double > Vector_double;
-
-
-template<> std::vector< double > &Writable< std::vector< double > >()
-{
-    return Vector_double;
-}
-
-
-std::vector< std::string > Vector_string;
-
-
-template<> std::vector< std::string > &Writable< std::vector< std::string > >()
-{
-    return Vector_string;
-}
-
-
-template< typename container_t >
-    static Durations CopyTimer( std::shared_ptr< container_t > container, int num_iters )
-{
-    const container_t &src = *container;
-    container_t &dst = Writable< container_t >();
-
-    std::function< Durations() > f = [src, &dst]()
-        {
-            container_t tmp;
-            tmp.swap( dst );
-
-            TimePoints start_times = Start();
-            dst = src;
-            return End( start_times );
-        };
-
-    return Time( f, num_iters );
-}
-
-
-template< typename container_t >
-    static Durations CopyOverhead( std::shared_ptr< container_t > container, int num_iters )
-{
-    const container_t &src = *container;
-    container_t &dst = Writable< container_t >();
-    dst.clear();
-
-    std::function< void() > f = [src, dst]()
-        {
-            if (src.empty() || !dst.empty()) std::terminate();
-        };
-
-    return Time( f, num_iters );
-}
-
-
-template< typename container_t >
-    static autotime::BenchTimers MakeCopyTimers( size_t n )
-{
-    using namespace std::placeholders;
-    using value_t = typename container_t::value_type;
-    std::shared_ptr< value_t[] > data = MakeData< value_t >( n );
-    std::shared_ptr< container_t > container{
-        new container_t{ Insert< container_t >( data.get(), n ) } };
-
-    return {
-            std::bind( &CopyTimer< container_t >, container, _1 ),
-            std::bind( &CopyOverhead< container_t >, container, _1 )
-        };
-}
-
-
 template<> autotime::BenchTimers MakeTimers< Benchmark::set_int32_copy1 >()
 {
     return MakeCopyTimers< std::set< int32_t > >( 1 << 0 );
@@ -658,44 +660,6 @@ template<> autotime::BenchTimers MakeTimers< Benchmark::set_double_copy64k >()
 template<> autotime::BenchTimers MakeTimers< Benchmark::set_string_copy64k >()
 {
     return MakeCopyTimers< std::set< std::string > >( 1 << 16 );
-}
-
-
-template< typename container_t >
-    static Durations DestroyTimer( std::shared_ptr< container_t > container, int num_iters )
-{
-    const container_t &src = *container;
-    container_t &dst = Writable< container_t >();
-
-    std::function< Durations() > f = [src, &dst]()
-        {
-            dst = src;
-
-            TimePoints start_times = Start();
-            {
-                container_t tmp;
-                tmp.swap( dst );
-            }
-            return End( start_times );
-        };
-
-    return Time( f, num_iters );
-}
-
-
-template< typename container_t >
-    static autotime::BenchTimers MakeDestroyTimers( size_t n )
-{
-    using namespace std::placeholders;
-    using value_t = typename container_t::value_type;
-    std::shared_ptr< value_t[] > data = MakeData< value_t >( n );
-    std::shared_ptr< container_t > container{
-        new container_t{ Insert< container_t >( data.get(), n ) } };
-
-    return {
-            std::bind( &DestroyTimer< container_t >, container, _1 ),
-            std::bind( &CopyOverhead< container_t >, container, _1 )
-        };
 }
 
 
@@ -759,10 +723,45 @@ template<> autotime::BenchTimers MakeTimers< Benchmark::set_string_destroy64k >(
 }
 
 
-
 ////////////////////////////////////////////////////////////
 // Category::std_vector:
 ////////////////////////////////////////////////////////////
+
+std::vector< int32_t > Vector_int32;
+std::vector< int64_t > Vector_int64;
+std::vector< float > Vector_float;
+std::vector< double > Vector_double;
+std::vector< std::string > Vector_string;
+
+
+template<> std::vector< int32_t > &Writable< std::vector< int32_t > >()
+{
+    return Vector_int32;
+}
+
+
+template<> std::vector< int64_t > &Writable< std::vector< int64_t > >()
+{
+    return Vector_int64;
+}
+
+
+template<> std::vector< float > &Writable< std::vector< float > >()
+{
+    return Vector_float;
+}
+
+
+template<> std::vector< double > &Writable< std::vector< double > >()
+{
+    return Vector_double;
+}
+
+
+template<> std::vector< std::string > &Writable< std::vector< std::string > >()
+{
+    return Vector_string;
+}
 
 template<> autotime::BenchTimers MakeTimers< Benchmark::vec_int32_insert16 >()
 {
