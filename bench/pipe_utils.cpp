b348df002e99a5680f4a093728ca1c07074c9378
Matt <mattgruenke@users.noreply.github.com>
2023-06-25 04:24:10 -0400

    Added pipe_utils.{hpp,cpp} - Pipe-related utility functions.

---
+++ bench/pipe_utils.cpp
@@ -0,0 +1,55 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//  Copyright Matthew A. Gruenke 2022.
+//
+//  Distributed under the Boost Software License, Version 1.0.
+//  (See accompanying file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+//! Pipe-related utility functions.
+/*! @file
+
+    See pipe_utils.hpp, for details.
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "pipe_utils.hpp"
+
+#include "error_utils.hpp"
+#include "file_utils.hpp"
+
+#include <limits>
+#include <stdexcept>
+#include <string>
+
+#include <fcntl.h>
+
+
+namespace bench
+{
+
+
+size_t GetMaxPipeSize()
+{
+    // Cache the value for subsequent calls.
+    static const int result = ReadFromFile< int >( "/proc/sys/fs/pipe-max-size" );
+
+    if (result <= 0) throw std::runtime_error(
+            "Invalid pipe max size: " + std::to_string( result ) );
+
+    return static_cast< size_t >( result );
+}
+
+
+void SetPipeSize( int fds[2], size_t size )
+{
+    if (size > std::numeric_limits< int >::max()) throw std::out_of_range(
+        "Requested size is too large even to attempt: " + std::to_string( size ) );
+
+    int actual = fcntl( fds[1], F_SETPIPE_SZ, (int) size );
+    if (actual < 0) throw_system_error( errno, "Set pipe size" );
+    else if (actual < (int) size) throw std::runtime_error(
+        "Set pipe size failed: " + std::to_string( actual ) + " < " + std::to_string( size ) );
+}
+
+
+} // namespace bench
+
---
+++ bench/pipe_utils.hpp
@@ -0,0 +1,47 @@
+////////////////////////////////////////////////////////////////////////////////
+//  Copyright Matthew A. Gruenke 2022.
+//
+//  Distributed under the Boost Software License, Version 1.0.
+//  (See accompanying file LICENSE_1_0.txt or copy at
+//   http://www.boost.org/LICENSE_1_0.txt)
+//
+//! Pipe-related utility functions.
+/*! @file
+
+*/
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef BENCH_PIPE_UTILS_HPP
+#define BENCH_PIPE_UTILS_HPP
+
+
+#include <cstddef>
+
+
+namespace bench
+{
+
+
+    //! Returns the system's maximum supported pipe buffer size.
+size_t GetMaxPipeSize();
+
+
+    //! Attempts to set the buffer size of a pipe.
+    /*!
+        @throws std::out_of_range if the requested size is too large to attempt.
+
+        @throws std::system_error if the fcntl() fails.
+
+        @throws std::runtime_error if the actual size is less than requested.
+    */
+void SetPipeSize(
+    int fds[2],
+    size_t size
+);
+
+
+} // namespace bench
+
+
+#endif  // ndef BENCH_PIPE_UTILS_HPP
+
