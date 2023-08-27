b53dedf9b5a7de53a83d251f3f348718420bac00
Matt <mattgruenke@users.noreply.github.com>
2023-08-27 06:54:59 -0400

    Added more file-related utilities.
    Additions are simply wrapper functions with error-checking + struct ScopedFile.
    
    Renamed OpenInputFile() to OpenIFStream().

--- bench/file_utils.cpp
+++ bench/file_utils.cpp
@@ -13,17 +13,116 @@
 
 #include "file_utils.hpp"
 
+#include <array>
+
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <boost/filesystem.hpp>
+
+#include "error_utils.hpp"
+
+
+namespace filesystem = boost::filesystem;
 
 namespace bench
 {
 
 
-std::ifstream OpenInputFile( const std::string &filename, std::ifstream::openmode mode )
+int OpenFile( const char *filename, int flags )
+{
+    static constexpr mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
+    int fd = open( filename, flags, mode );
+    if (fd < 0) throw_system_error( errno, "open()" );
+
+    return fd;
+}
+
+
+void Close( int fd )
+{
+    if (fd >= 0 && close( fd )) throw_system_error( errno, "close()" );
+}
+
+
+void Unlink( const char *filename )
+{
+    if (unlink( filename )) throw_system_error( errno, "unlink()" );
+}
+
+
+size_t Write( int fd, const void *buf, size_t count )
+{
+    ssize_t written = write( fd, buf, count );
+    if (written < 0)
+    {
+        if (errno == EAGAIN || errno == EINTR) written = 0;
+        else throw_system_error( errno, "write()" );
+    }
+
+    return static_cast< size_t >( written );
+}
+
+
+static constexpr size_t Data_size = 4096;
+
+
+static std::array< uint8_t, Data_size > MakeData()
+{
+    std::array< uint8_t, Data_size > array;
+    for (uint8_t &element: array) element = static_cast< uint8_t >( random() & 0xff );
+
+    return array;
+}
+
+
+void FillFile( int fd, size_t size )
+{
+    static const auto array = MakeData();
+    size_t remain = size;
+    while (remain > 0) remain -= Write( fd, array.data(), std::min( array.size(), remain ) );
+}
+
+
+std::ifstream OpenIFStream( const std::string &filename, std::ifstream::openmode mode )
 {
     std::ifstream stream;
     stream.exceptions( std::ifstream::badbit | std::ifstream::failbit );
     stream.open( filename, mode | std::ifstream::in );
     return stream;
+}
+
+
+// struct ScopedFile:
+const int ScopedFile::flags = O_CREAT | O_RDWR;
+
+
+ScopedFile::ScopedFile()
+:
+    ScopedFile( filesystem::unique_path().native() )
+{
+}
+
+
+ScopedFile::ScopedFile( const std::string &filename )
+:
+    filename( filename ),
+    fd( OpenFile( filename.c_str(), flags ) )
+{
+}
+
+
+ScopedFile::~ScopedFile()
+{
+    this->close();
+    Unlink( filename.c_str() );
+}
+
+
+void ScopedFile::close()
+{
+    if (fd >= 0) Close( fd );
+    fd = -1;
 }
 
 
--- bench/file_utils.hpp
+++ bench/file_utils.hpp
@@ -23,6 +23,50 @@ namespace bench
 {
 
 
+    //! Wraps open().
+    /*!
+        @throws std::system_error upon failure.
+    */
+int OpenFile( const char *filename, int flags );
+
+
+    //! Wraps close().
+    /*!
+        @throws std::system_error upon failure.
+    */
+void Close( int fd );
+
+
+    //! Wraps unlink().
+    /*!
+        @throws std::system_error upon failure.
+    */
+void Unlink( const char *filename );
+
+
+    //! Wraps write().
+    /*!
+        @throws std::system_error upon failure.
+
+        @returns the number of bytes written.
+
+        @note
+        Returns 0, in case of EINTR or EAGAIN.
+    */
+size_t Write( int fd, const void *buf, size_t count );
+
+
+    //! Fills the file with random data.
+    /*!
+        @throws std::system_error upon failure.
+
+        @note
+        This performs multiple writes and does some extra work, during the
+        first call.
+    */
+void FillFile( int fd, size_t size );
+
+
     //! Default supplementary mode bits value (none).
 static constexpr std::ios_base::openmode OpenMode0{};
 
@@ -34,7 +78,7 @@ static constexpr std::ios_base::openmode OpenMode0{};
         The mode parameter is for specifying bits besides std::ios_base::in,
         if desired.  Typically, this would just be std::ios_base::binary.
     */
-std::ifstream OpenInputFile(
+std::ifstream OpenIFStream(
     const std::string &filename,                //!< Absolute or relative path.
     std::ifstream::openmode mode = OpenMode0    //!< Additional mode bits.
 );
@@ -53,10 +97,40 @@ value_type ReadFromFile(
 )
 {
     value_type value;
-    std::ifstream stream{ OpenInputFile( filename ) };
+    std::ifstream stream = OpenIFStream( filename );
     stream >> value;
     return value;
 }
+
+
+    //! Binds a file & its handle to a scope.
+    /*!
+        Upon construction, the file is opened (and created, if necessary).
+        Upon destruction, it's closed (if still open) and unlinked.
+        Thus, passing the name of an existing file will result in its
+        eventual deletion.  Otherwise, instantiating the class will result
+        in an entirely emphemeral (but, otherwise ordinary) file.
+
+        @throws std::system_error upon failure.
+    */
+struct ScopedFile
+{
+    static const int flags;
+    const std::string filename;
+    int fd = -1;
+
+        //! Instantiates with a uniquely-named file in the CWD.
+    ScopedFile();
+
+        //! Opens the specified filename for read/write, creating if necessary.
+    explicit ScopedFile( const std::string &filename );
+
+        //! Closes (if open) and unlinks the file.
+    ~ScopedFile();
+
+        //! Closes the file (if open).
+    void close();
+};
 
 
 } // namespace bench
